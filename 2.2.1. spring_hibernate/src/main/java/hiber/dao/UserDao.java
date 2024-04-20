5fc7ff418729a44bcea8f6ed4e29b88223925081
pav3433 <gubkin.14@yandex.ru>
2024-04-20 09:17:05 +0300

    change2

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
@@ -5,7 +5,9 @@ import hiber.model.User;
 import java.util.List;
 
 public interface UserDao {
-   void add(User user);
-   List<User> listUsers();
-   List<User> getUserByCar(String model, int series);
+    void add(User user);
+
+    List<User> listUsers();
+
+    List<User> getUserByCar(String model, int series);
 }
