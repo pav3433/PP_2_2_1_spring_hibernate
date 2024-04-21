0805b1aa133b4881cbd2f518bfddb89503bc2b72
pav3433 <gubkin.14@yandex.ru>
2024-04-21 14:41:37 +0300

    change4

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -11,9 +11,11 @@ import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
-
-    @Autowired
     private SessionFactory sessionFactory;
+    @Autowired
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public void add(User user) {
